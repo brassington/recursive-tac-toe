53619bfb0252f770d38dbfd0318232559be0545f
J. Andrew Brassington <jabbrass@zoho.com>
2014-10-23 17:00:00 -0700

    Fix class on circle SVG

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var Game = function() {
   var target = undefined;
 
   var X = '<svg xmlns="http://www.w3.org/2000/svg" class="cancel" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 128 128" enable-background="new 0 0 128 128">    <path d="M72.282,64.867l40.057-40.057c2.299-2.299,2.299-6.018,0-8.313c-2.294-2.299-6.014-2.299-8.313,0L63.97,56.554  L23.908,16.497c-2.295-2.299-6.018-2.299-8.313,0c-2.295,2.295-2.295,6.014,0,8.313l40.061,40.057l-40.061,40.061  c-2.295,2.295-2.295,6.018,0,8.313c1.15,1.15,2.653,1.722,4.156,1.722c1.504,0,3.007-0.572,4.157-1.722L63.97,73.18l40.057,40.062   c1.15,1.15,2.653,1.722,4.156,1.722c1.509,0,3.012-0.572,4.157-1.722c2.299-2.295,2.299-6.018,0-8.313L72.282,64.867z"/> </svg>';
-  var O = '<svg xmlns="http://www.w3.org/2000/svg" class="Layer_1" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 128 128" enable-background="new 0 0 128 128"> <path d="M64,11.7c-28.9,0-52.3,23.5-52.3,52.3c0,28.9,23.5,52.4,52.3,52.4s52.3-23.5,52.3-52.4 C116.3,35.1,92.9,11.7,64,11.7z M64.3,104.2c-22.2,0-40.2-18-40.2-40.2s18-40.2,40.2-40.2s40.2,18,40.2,40.2 C104.5,86.2,86.4,104.2,64.3,104.2z" fill="#010101"/> </svg>';
+  var O = '<svg xmlns="http://www.w3.org/2000/svg" class="circle" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 128 128" enable-background="new 0 0 128 128"> <path d="M64,11.7c-28.9,0-52.3,23.5-52.3,52.3c0,28.9,23.5,52.4,52.3,52.4s52.3-23.5,52.3-52.4 C116.3,35.1,92.9,11.7,64,11.7z M64.3,104.2c-22.2,0-40.2-18-40.2-40.2s18-40.2,40.2-40.2s40.2,18,40.2,40.2 C104.5,86.2,86.4,104.2,64.3,104.2z" fill="#010101"/> </svg>';
 
   // playPiece tests the target of each click or touch to match a play on a subboard
   // DOM traversal is handled by references to parentNodes
